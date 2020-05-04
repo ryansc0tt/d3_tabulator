a379f464562f8a64f47f1d55b588b55ac9d8ed15
Ryan Middleton <ryan.s.middleton@gmail.com>
2020-05-04 17:24:08 -0500

    add basic sample

---
+++ samples/brooklyn_dogs/script/tabulate.js
@@ -0,0 +1,24 @@
+import d3_tabulator from './module/d3_tabulator.js';
+
+// data file
+const filenames = ['data/Brooklyn_Dog_Licensing_Dataset_2018.csv'];
+
+// tabulator obj
+const tabulator = new d3_tabulator();
+
+// append loading message
+$('body').append($('<div>').append($('<p>').text('loading...')));
+
+// load data
+tabulator.load_data(filenames).then(() => {
+	
+	// tabulate default view (based on filename)
+	let table = tabulator.tabulate_view(filenames[0]);
+	
+	// append to document
+	$('body div').append(table.node());
+
+	// remove loading message
+	$('body div p').remove();
+
+});
